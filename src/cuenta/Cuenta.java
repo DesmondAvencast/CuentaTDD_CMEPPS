8a80a4421f8df1017d1f45eb516ec9b46debfb58
H <H@DESKTOP-J55SRPI>
2021-11-13 09:57:24 +0100

    Cuenta Atributos y Metodos Vacios

---
+++ src/cuenta/Cuenta.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuenta;
+
+/**
+ *
+ * @author H
+ */
+public class Cuenta {
+
+    private double saldo;
+    private String nCuenta;
+    
+    public Cuenta(double saldo, String cuenta){
+        
+    }
+    
+    public void Retirada(double retirada){
+        
+    }
+    
+    public void Ingreso(double ingreso){
+        
+    }
+    
+    public int getSaldo(){
+        return 0;
+    }
+    
+    public String getCuenta(){
+        return "0";
+    }
+    
+}
